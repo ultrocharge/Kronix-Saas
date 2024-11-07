d8bee0d9c21adc4e06856e76aea8f30766aceffb
ultrocharge <oliver.elias011@gmail.com>
2024-11-07 18:44:08 +0000

    Completed

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./section/Header";
+import Hero from "./section/Hero";
+
+export default function App() {
+  return (
+    <div className="bg-background">
+      <div className="mx-auto max-w-7xl">
+        <Header />
+        <div>
+          <Hero />
+        </div>
+      </div>
+    </div>
+  );
+}
